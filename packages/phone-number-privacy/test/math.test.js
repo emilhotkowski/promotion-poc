bed81287dec77c97ec176265e2fb637a94f56a25
Emil Hotkowski <emil.hotkowski@gmail.com>
2021-07-29 14:13:54 +0200

    Test3

--- packages/phone-number-privacy/test/math.test.js
+++ packages/phone-number-privacy/test/math.test.js
@@ -3,7 +3,7 @@ const myMath = require("../index")
 describe("Math test", () => {
 
     it("Should add numbers correctly", () => {
-        expect(myMath.add(2, 2)).toEqual(4)
+        expect(myMath.add(2, 2)).toEqual(5)
     })
 
 });
